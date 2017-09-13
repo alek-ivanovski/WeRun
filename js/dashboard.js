f573465c518a8e8f7ddd82601efd6eb89211d6f1
Alek Ivanovski <alek.ivanovski96@gmail.com>
2017-09-13 18:58:00 +0200

    'dashboard'

---
+++ js/dashboard.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Alek Ivanovski on 9/13/2017.
+ */
+
+$(".add_comment1").click(function(){
+    var comment_new = $(this).parent().parent();
+    var comment = $('#form1').val();
+    $(comment_new).prev().append("<div class='media'>"+
+    "<div class='media-left media-top'>"+
+        "<img src='img/alek.jpg' class='media-object profile_picture img-circle'>"+
+        "</div>"+
+        "<div class='media-body'>"+
+        "<h5 class='media-heading profile_name'>Alek Ivanovski</h5>"+
+    "<p class='comment'>"+ comment +"</p>"+
+    "</div>"+
+    "</div>");
+    $('.form-control').val("");
+});
+
+$(".add_comment2").click(function(){
+    var comment_new = $(this).parent().parent();
+    var comment = $('#form2').val();
+    $(comment_new).prev().append("<div class='media'>"+
+        "<div class='media-left media-top'>"+
+        "<img src='img/alek.jpg' class='media-object profile_picture img-circle'>"+
+        "</div>"+
+        "<div class='media-body'>"+
+        "<h5 class='media-heading profile_name'>Alek Ivanovski</h5>"+
+        "<p class='comment'>"+ comment +"</p>"+
+        "</div>"+
+        "</div>");
+    $('.form-control').val("");
+});
